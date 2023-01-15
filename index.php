9c9e36d22a8162734827324c160ee3c39829ee78
bryan jinez <bryanjinez200027@gmail.com>
2023-01-14 19:27:56 -0500

    Se modifico el login en el archivo index.php y se agrego un control en el boton para el login en el archivo index.php de la carpeta modulos

--- index.php
+++ index.php
@@ -39,9 +39,9 @@ include("modulos/index.php");
     <div class="card-body login-card-body">
       <p class="login-box-msg">Inicio de Sesion: </p>
 
-      <form action="vistaPanel.php" method="post">
+      <form action="index.php" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" name="txtEmail" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -49,7 +49,7 @@ include("modulos/index.php");
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="txtPassword" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -62,7 +62,7 @@ include("modulos/index.php");
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" name="btnLogin" class="btn btn-primary btn-block">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
--- modulos/index.php
+++ modulos/index.php
@@ -1,3 +1,9 @@
 <?php
     echo "Hola soy index en modulos";
+
+    if(isset($_POST["btnLogin"])){
+
+        echo "<br/> hay que validar el correo y la contraseña ";
+    }
+
 ?>
