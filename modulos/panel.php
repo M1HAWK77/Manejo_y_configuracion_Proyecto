28e5e5aa486e2e3d5676fc8e93a53299312e4ac4
SebasTix02 <sebastixi2002@gmail.com>
2023-01-19 21:49:11 -0500

    Cambio en el card de Compras Concretadas

--- modulos/panel.php
+++ modulos/panel.php
@@ -1,3 +1,12 @@
 <?php
 include("global/sesiones.php");
+include("global/conexionBD.php");
+
+$sentenciaSQL = $pdo->prepare("SELECT count(*) totalVentas FROM ventas");
+
+$sentenciaSQL->execute();
+
+$registro=$sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+$totalVentas = $registro['totalVentas'];
+
 ?>
--- vistaPanel.php
+++ vistaPanel.php
@@ -30,14 +30,12 @@
           <!-- small box -->
           <div class="small-box bg-info">
             <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
+              <h3><?php echo $totalVentas;?></h3>
+              <p>Ordenes Concretadas</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
