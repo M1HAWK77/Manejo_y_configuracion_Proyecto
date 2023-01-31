32270324bd2e11b8e01d584bb420396a49bf417f
unknown <bryanjinez200027@gmail.com>
2023-01-31 00:17:12 -0500

    Se agrego cuadro de descripción para productos

--- vistaProductos.php
+++ vistaProductos.php
@@ -1,17 +1,18 @@
-<?php include("modulos/productos.php");?>
+<?php include("modulos/productos.php"); ?>
 <?php include("cabecera.php") ?>
 <?php include("global/config.php") ?>
 <?php include("global/conexionBD.php") ?>
-<head>
-    <meta charse="UTF-8">
-    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tienda</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+<head>
+  <meta charse="UTF-8">
+  <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tienda</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 </head>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -33,160 +34,167 @@
   </div>
   <!-- /.content-header -->
   <div class="card card-info">
-  <div class="alert alert-success">
-        Pantalla de mensaje ...
-        <a href="#" class="badge badge-success">Ver carrito</a>
+    <div class="alert alert-success">
+      Pantalla de mensaje ...
+      <a href="#" class="badge badge-success">Ver carrito</a>
     </div>
     <div class="row">
 
 
 
-        <?php
-        $sentencia = $pdo->prepare("SELECT * FROM `productos`");
-        $sentencia->execute();
-        $listaProductos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
-        // print_r($listaProductos);
-        ?>
-        <?php foreach ($listaProductos as $producto) { ?>
-            <div class="col-3">
-                <div class="card">
-                    <img tittle="<?php echo $producto['nombreP']; ?>" alt="<?php echo $producto['nombreP']; ?>" class="card-img-top" src="<?php echo $producto['img']; ?>">
-                    <div class="card-body">
-                        <span><?php echo $producto['nombreP']; ?></span>
-                        <h5 class="" card-tittle>$<?php echo $producto['precioP']; ?></h5>
-                        <p class="card-text">Descripcion</p>
-
-                        <button class="btn btn-primary" name="btnAccion" value="Agregar" type="submit"> Agregar carrito </button>
-
-                    </div>
-                </div>
+      <?php
+      $sentencia = $pdo->prepare("SELECT * FROM `productos`");
+      $sentencia->execute();
+      $listaProductos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+      // print_r($listaProductos);
+      ?>
+      <?php foreach ($listaProductos as $producto) { ?>
+       <div class="col-3">
+        <div class="card">
+          <img tittle="<?php echo $producto['nombreP']; ?>" alt="<?php echo
+                                                                  $producto['nombreP']; ?>" class="card-img-top" src="<?php echo $producto['img']; ?>" data-toggle="popover" data-trigger="hover" data-content="<?php echo $producto['descripcion']; ?>">
+          
+            <div class="card-body">
+              <span><?php echo $producto['nombreP']; ?></span>
+              <h5 class="" card-tittle>$<?php echo $producto['precioP']; ?></h5>
+              <p class="card-text">Descripcion</p>
+              <form action="" method="post">
+                <input type="text" name="id" id="id" value="<?php echo openssl_encrypt($producto['id'], COD, KEY); ?>">
+                <input type="text" name="nombre" id="nombre" value="<?php echo openssl_encrypt($producto['nombreP'], COD, KEY); ?>">
+                <input type="text" name="precio" id="precio" value="$<?php echo openssl_encrypt($producto['precioP'], COD, KEY); ?>">
+                <input type="text" name="cantidad" id="cantidad" value="<?php echo openssl_encrypt(1, COD, KEY); ?>">
+                <button class="btn btn-primary" name="btnAccion" value="Agregar" type="submit">
+                  Agregar carrito </button>
+              </form>
             </div>
-        <?php } ?>
+          </div>
+        </div>
+      <?php } ?>
     </div>
 
     <script>
-    $(function () {
-  $('[data-toggle="popover"]').popover()
-});
-</script>
+      $(function() {
+        $('[data-toggle="popover"]').popover()
+      });
+    </script>
 
-  <!-- Main content -->
-  <section class="content">
-    <div class="container-fluid">
-     
-      <!-- Formulario de productos -->
-      <div class="card card-info">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+
+        <!-- Formulario de productos -->
+        <div class="card card-info">
+          <div class="card-header">
+            <h3 class="card-title">Horizontal Form</h3>
+          </div>
+          <!-- /.card-header -->
+          <!-- form start -->
+          <form class="form-horizontal">
+            <div class="card-body">
+              <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="form-group row">
+                <div class="offset-sm-2 col-sm-10">
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck2">
+                    <label class="form-check-label" for="exampleCheck2">Remember me</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              <button type="submit" class="btn btn-info">Sign in</button>
+              <button type="submit" class="btn btn-default float-right">Cancel</button>
+            </div>
+            <!-- /.card-footer -->
+          </form>
+        </div>
+
+
+        <!-- Fin formulario de productos -->
+
+
+        <!-- Main row -->
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Horizontal Form</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form class="form-horizontal">
-                <div class="card-body">
-                  <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                <h3 class="card-title">Responsive Hover Table</h3>
+                <div class="card-tools">
+                  <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                    <div class="input-group-append">
+                      <button type="submit" class="btn btn-default">
+                        <i class="fas fa-search"></i>
+                      </button>
                     </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <div class="offset-sm-2 col-sm-10">
-                      <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck2">
-                        <label class="form-check-label" for="exampleCheck2">Remember me</label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-info">Sign in</button>
-                  <button type="submit" class="btn btn-default float-right">Cancel</button>
-                </div>
-                <!-- /.card-footer -->
-              </form>
-            </div>
-
-
-      <!-- Fin formulario de productos -->
-
-
-      <!-- Main row -->
-      <div class="row">
-        <div class="col-12">
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Responsive Hover Table</h3>
-              <div class="card-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                  <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                  <div class="input-group-append">
-                    <button type="submit" class="btn btn-default">
-                      <i class="fas fa-search"></i>
-                    </button>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div class="card-body table-responsive p-0">
-              <table class="table table-hover text-nowrap">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>User</th>
-                    <th>Date</th>
-                    <th>Status</th>
-                    <th>Reason</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>183</td>
-                    <td>John Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-success">Approved</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                  </tr>
-                  <tr>
-                    <td>219</td>
-                    <td>Alexander Pierce</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-warning">Pending</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                  </tr>
-                  <tr>
-                    <td>657</td>
-                    <td>Bob Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-primary">Approved</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                  </tr>
-                  <tr>
-                    <td>175</td>
-                    <td>Mike Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-danger">Denied</span></td>
-                    <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>User</th>
+                      <th>Date</th>
+                      <th>Status</th>
+                      <th>Reason</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>219</td>
+                      <td>Alexander Pierce</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-warning">Pending</span></td>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>657</td>
+                      <td>Bob Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-primary">Approved</span></td>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                    <tr>
+                      <td>175</td>
+                      <td>Mike Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-danger">Denied</span></td>
+                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
             </div>
 
           </div>
-
         </div>
-      </div>
-      <!-- /.row (main row) -->
-    </div><!-- /.container-fluid -->
-  </section>
-  <!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
-<?php include("footer.php") ?>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include("footer.php") ?>
