bc9113d3b665ce4b18861eac45e9a4f40b9ffa30
SebasTix02 <sebastixi2002@gmail.com>
2023-02-01 21:12:15 -0500

    Problema solucionado dentro de la cabecera

--- cabecera.php
+++ cabecera.php
@@ -45,7 +45,7 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
           </li>
           <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.php" class="nav-link">Home</a>
+            <a href="vistaPanel.php" class="nav-link">Home</a>
           </li>
           <li class="nav-item d-none d-sm-inline-block">
             <a href="mostrarCarrito.php" class="nav-link">Carrito(<?php
@@ -115,7 +115,7 @@
       <!-- Main Sidebar Container -->
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.php" class="brand-link">
+        <a href="vistaPanel.php" class="brand-link">
           <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">AdminLTE 3</span>
         </a>
